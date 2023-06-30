a269f08e20458d462c27ffd2f0ccd15be82d6daf
Ray Lee <ray.lee@osckorea.com>
2023-06-30 15:35:57 +0900

    V 1.0.0 : Add schema & sample data

--- src/main/resources/database/master/data-master.sql
+++ src/main/resources/database/master/data-master.sql
@@ -1,11 +1,33 @@
-INSERT INTO user(user_id) VALUES(1);
-INSERT INTO user(user_id) VALUES(2);
-INSERT INTO user(user_id) VALUES(3);
-INSERT INTO user(user_id) VALUES(4);
-INSERT INTO user(user_id) VALUES(5);
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser1","Merry",    "set0000","010-1234-4132","서울 서초구 잠원로8길 35",            "403호","2023-06-30");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser2","John",     "pwd1234","010-2534-4131","서울 서초구 나루터로12길 18 수인빌딩","802호","2023-06-02");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser3","Petter",   "pwd5674","010-3257-5322","서울 서초구 강남대로95길 19-4",       "301호","2023-06-04");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser4","Ray",      "pwd0122","010-4356-1331","서울 강남구 학동로12길 18",           "710호","2023-06-23");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser5","Christina","pwd4678","010-4536-3112","서울 강남구 논현로 627 유현빌딩",     "410호","2023-06-11");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser6","Chris",    "pwd4556","010-6436-3213","서울 강남구 언주로 506",              "101호","2023-06-20");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser7","Thomas",   "pwd7983","010-5343-1231","서울 강남구 영동대로71길 24",         "901호","2023-06-25");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser8","Jenny",    "pwd10549","010-5435-3127","서울 용산구 대사관로30나길 32",       "506호","2023-06-18");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser9","Jack",     "pwd0524","010-4324-4310","서울 용산구 독서당로14길 62",         "105호","2023-06-10");
+INSERT INTO user(user_id,user_name,user_password,user_tel,user_address,detail_address,created_date) VALUES ("TestUser10","Parker",  "pwd12394","010-6783-6547","서울 용산구 녹사평대로 66",           "1002호","2023-06-14");
 
-INSERT INTO board(board_id) VALUES(6);
-INSERT INTO board(board_id) VALUES(7);
-INSERT INTO board(board_id) VALUES(8);
-INSERT INTO board(board_id) VALUES(9);
-INSERT INTO board(board_id) VALUES(10);
+INSERT INTO address(address,address_zipcode) VALUES ("서울 서초구 잠원로8길 35","06518");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 서초구 나루터로12길 18 수인빌딩","06527");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 서초구 강남대로95길 19-4","06530");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 강남구 학동로12길 18","06113");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 강남구 논현로 627 유현빌딩","06117");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 강남구 언주로 506","06152");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 강남구 영동대로71길 24","06187");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 용산구 대사관로30나길 32","04409");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 용산구 독서당로14길 62","04410");
+INSERT INTO address(address,address_zipcode) VALUES ("서울 용산구 녹사평대로 66","04398");
+
+
+INSERT INTO board(board_title,board_contents,write_id,modify_id,write_date) VALUES("Test Board Title 1","Test Board Contents","TestUser1","TestUser10","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,modify_id,write_date) VALUES("Test Board Title 2","Test Board Contents","TestUser2","TestUser9","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,modify_id,write_date) VALUES("Test Board Title 3","Test Board Contents","TestUser3","TestUser8","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,modify_id,write_date) VALUES("Test Board Title 4","Test Board Contents","TestUser4","TestUser7","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,modify_id,write_date) VALUES("Test Board Title 5","Test Board Contents","TestUser5","TestUser6","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,write_date) VALUES("Test Board Title 6","Test Board Contents","TestUser6","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,write_date) VALUES("Test Board Title 7","Test Board Contents","TestUser7","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,write_date) VALUES("Test Board Title 8","Test Board Contents","TestUser9","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,write_date) VALUES("Test Board Title 9","Test Board Contents","TestUser9","2023-06-30");
+INSERT INTO board(board_title,board_contents,write_id,write_date) VALUES("Test Board Title 10","Test Board Contents","TestUser10","2023-06-30");
--- src/main/resources/database/slave/schema-slave.sql
+++ src/main/resources/database/slave/schema-slave.sql
@@ -1,10 +1,41 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS address;
 DROP TABLE IF EXISTS board;
 
-CREATE TABLE user(
-    user_id BIGINT NOT NULL PRIMARY KEY
+CREATE TABLE IF NOT EXISTS user (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id VARCHAR(50) NOT NULL,
+    user_name VARCHAR(40) NOT NULL,
+    user_password VARCHAR(60) NOT NULL,
+    user_tel VARCHAR(50) NOT NULL,
+    user_address VARCHAR(100) NOT NULL,
+    detail_address VARCHAR(30) NOT NULL,
+    created_date DATETIME NOT NULL,
+    modified_date DATETIME NULL,
+
+    PRIMARY KEY (user_id),
+
+    UNIQUE KEY (id)
 );
 
-CREATE TABLE board(
-    board_id BIGINT NOT NULL PRIMARY KEY
+CREATE TABLE IF NOT EXISTS address(
+    address VARCHAR(100) NOT NULL,
+    address_zipcode VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (address)
+);
+
+CREATE TABLE IF NOT EXISTS board(
+    board_id BIGINT NOT NULL AUTO_INCREMENT,
+
+    board_title VARCHAR(50) NOT NULL,
+    board_contents VARCHAR(200) NOT NULL,
+
+    write_id VARCHAR(50) NOT NULL,
+    modify_id VARCHAR(50) NULL,
+
+    write_date DATETIME NOT NULL,
+    modify_date DATETIME NULL,
+
+    PRIMARY KEY (board_id)
 );
