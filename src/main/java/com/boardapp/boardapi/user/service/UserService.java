c4796f8afc562a7e35c2e0bfd60788b1daca6443
Ray Lee <ray.lee@osckorea.com>
2023-06-30 11:23:32 +0900

    V 1.0.0 : Add user domain requirement files

--- src/main/java/com/boardapp/boardapi/user/service/UserService.java
+++ src/main/java/com/boardapp/boardapi/user/service/UserService.java
@@ -1,5 +1,19 @@
 package com.boardapp.boardapi.user.service;
 
-public class UserService {
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.boardapp.boardapi.common.mapper.slave.SlaveDatabaseMapper;
+import com.boardapp.boardapi.user.model.User;
 
+@Service
+public class UserService {
+    private final SlaveDatabaseMapper slaveDatabaseMapper;
+
+    public UserService(SlaveDatabaseMapper slaveDatabaseMapper) {
+        this.slaveDatabaseMapper = slaveDatabaseMapper;
+    }
+
+    public List<User> getUser() throws Exception {
+        return this.slaveDatabaseMapper.getAllUser();
+    }
 }
