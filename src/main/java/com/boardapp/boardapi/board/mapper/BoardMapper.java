23cdbb485266cd53877db456e3169a9d540e4306
Ray Lee <ray.lee@osckorea.com>
2023-07-17 14:23:17 +0900

    V 1.0.0 : MyBatis use Mapper

--- src/main/java/com/boardapp/boardapi/board/mapper/BoardMapper.java
+++ src/main/java/com/boardapp/boardapi/board/mapper/BoardMapper.java
@@ -1,9 +1,11 @@
 package com.boardapp.boardapi.board.mapper;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import com.boardapp.boardapi.board.model.Board;
 
+@Mapper
 public interface BoardMapper {
     List<Board> findAllBoards();
 
