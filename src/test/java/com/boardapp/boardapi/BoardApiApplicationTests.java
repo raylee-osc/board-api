fa3171f8ccf32f478404b9f22d62e145c451768a
Ray Lee <ray.lee@osckorea.com>
2023-07-17 21:07:59 +0900

    V 1.0.0 : Remove lib

--- src/test/java/com/boardapp/boardapi/BoardApiApplicationTests.java
+++ src/test/java/com/boardapp/boardapi/BoardApiApplicationTests.java
@@ -3,7 +3,6 @@ package com.boardapp.boardapi;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
