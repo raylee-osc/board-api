0037a18edefa911832401ce0e76adc96e0e5f118
Ray Lee <ray.lee@osckorea.com>
2023-07-17 17:58:30 +0900

    v 1.0.0

--- src/main/java/com/boardapp/boardapi/board/dao/BoardDaoImpl.java
+++ src/main/java/com/boardapp/boardapi/board/dao/BoardDaoImpl.java
@@ -11,31 +11,31 @@ import lombok.RequiredArgsConstructor;
 public class BoardDaoImpl implements BoardDao {
     private final SqlSessionTemplate sqlSessionTemplate;
 
-    private static final String namespace = "BoardDao";
+    private static final String NAMESPACE = "BoardDao";
 
     @Override
     public List<Board> findAllBoards() {
-        return this.sqlSessionTemplate.selectList(namespace + ".findAllBoards");
+        return this.sqlSessionTemplate.selectList(NAMESPACE + ".findAllBoards");
     }
 
     @Override
     public Board findByBoardId(Long boardId) {
-        return this.sqlSessionTemplate.selectOne(namespace + ".findByBoardId", boardId);
+        return this.sqlSessionTemplate.selectOne(NAMESPACE + ".findByBoardId", boardId);
     }
 
     @Override
     public Integer saveBoard(Board board) {
-        return this.sqlSessionTemplate.insert(namespace + ".saveBoard", board);
+        return this.sqlSessionTemplate.insert(NAMESPACE + ".saveBoard", board);
     }
 
     @Override
     public Integer updateBoard(Board board) {
-        return this.sqlSessionTemplate.update(namespace + ".updateBoard", board);
+        return this.sqlSessionTemplate.update(NAMESPACE + ".updateBoard", board);
     }
 
     @Override
     public Integer deleteBoard(Long boardId) {
-        return this.sqlSessionTemplate.delete(namespace + ".deleteBoard", boardId);
+        return this.sqlSessionTemplate.delete(NAMESPACE + ".deleteBoard", boardId);
     }
 
 }
