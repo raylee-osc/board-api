ff0030e1a32a6e97f834db2b0ca59bc0943972b3
Ray Lee <ray.lee@osckorea.com>
2023-07-20 11:05:54 +0900

    V 1.0.0 : Rewrite Table schema & dummy data

--- src/main/resources/database/schema-mysql.sql
+++ src/main/resources/database/schema-mysql.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS board;
-DROP TABLE IF EXISTS user;
 
 CREATE TABLE IF NOT EXISTS board(
     board_id BIGINT NOT NULL AUTO_INCREMENT,
@@ -14,18 +13,4 @@ CREATE TABLE IF NOT EXISTS board(
     modify_date DATETIME NULL,
 
     PRIMARY KEY (board_id)
-);
-
-CREATE TABLE IF NOT EXISTS user (
-    index_id BIGINT NOT NULL AUTO_INCREMENT,
-    user_id VARCHAR(50) NOT NULL,
-    user_name VARCHAR(40) NOT NULL,
-    user_password VARCHAR(60) NOT NULL,
-    user_phonenumber VARCHAR(50) NOT NULL,
-    created_date DATETIME NOT NULL,
-    modified_date DATETIME NULL,
-
-    PRIMARY KEY (user_id),
-
-    UNIQUE KEY (index_id)
 );
